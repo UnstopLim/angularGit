faa896430395ab39f50746ecc3e3c8010800735d
Limber Mamani Canaza <draw.redmi8@gmail.com>
2024-02-19 15:47:56 -0400

    mi cuarto commit desde 1.2 desde  githab local

---
+++ src/app/componente/primer/primer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primer',
+  standalone: true,
+  imports: [],
+  templateUrl: './primer.component.html',
+  styleUrl: './primer.component.css'
+})
+export class PrimerComponent {
+
+}
